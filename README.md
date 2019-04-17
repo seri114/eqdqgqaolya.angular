3f3ecee2b6a7095adb8b25d60939f1e052d80f6e
seri114 <seri114@gmail.com>
2019-04-17 23:41:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# eqdqgqaolya.angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/angular/eqdqgqaolya)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  title2 = 'Tour of Heroes2';
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
